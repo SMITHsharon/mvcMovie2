5b08f87e8657312521aa3765f274ac3d9865724e
SMITHsharon <sharon.e.smith2@gmail.com>
2018-03-17 23:14:30 -0500

    format Date output in Model + add Search on Title or Genre

--- mvcMovie/Models/Movie.cs
+++ mvcMovie/Models/Movie.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
@@ -11,8 +8,11 @@ namespace MvcMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
